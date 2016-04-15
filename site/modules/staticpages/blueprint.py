d2f1ebb46bd55ba8f47def9fdf5c9edaff5a1b8a
Fox Wilson <fwilson@fwilson.me>
2016-04-15 09:56:59 -0400

    fix staticpages

--- site/modules/staticpages/blueprint.py
+++ site/modules/staticpages/blueprint.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, render_template
 
-staticpages = Blueprint("staticpages", __name__, template_folder="templates", url_prefix="/")
+staticpages = Blueprint("staticpages", __name__, template_folder="templates", url_prefix="")
 
-@staticpages.route("/")
+@staticpages.route("/", defaults={"page": "index"})
 @staticpages.route("/<page>/")
-def show_staticpage(page="index"):
+def show_staticpage(page):
+    print(page)
     return render_template("{}.html".format(page))
