67732ab8d46733921c1d89aaa3236cb53b90c3e7
Fox Wilson <fwilson@fwilson.me>
2016-04-15 11:35:05 -0400

    stripe

--- site/config/dev.py
+++ site/config/dev.py
@@ -1,3 +1,9 @@
+import os
+
 SEND_EMAIL = False
 APP_NAME = "Teens for Teens"
 SECRET_KEY = "key"
+
+SECRET_KEY = os.getenv("SECRET_KEY", os.urandom(32))
+STRIPE_KEY_SECRET = os.getenv("STRIPE_KEY_SECRET", "")
+STRIPE_KEY_PUBLIC = os.getenv("STRIPE_KEY_PUBLIC", "")
