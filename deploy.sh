0f864db8e2c90af1fef83d711969a2eb8cc3937b
Fox Wilson <fwilson@fwilson.me>
2016-04-15 21:17:46 -0400

    add homepage link

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 git clone $(pwd) /tmp/tft
-(cd /tmp/tft
-git checkout testing
-rsync -rv --exclude .git site nfsn.sh fwilson42_tft-test@ssh.phx.nearlyfreespeech.net:/home/protected)
+
+if test $1 = test; then
+    (cd /tmp/tft
+    git checkout testing
+    rsync -rv --exclude .git site nfsn.sh fwilson42_tft-test@ssh.phx.nearlyfreespeech.net:/home/protected)
+else
+    if test $1 = prod; then
+        (cd /tmp/tft
+        git checkout master
+        rsync -rv --exclude .git site nfsn.sh fwilson42_tft-production@ssh.phx.nearlyfreespeech.net:/home/protected)
+    fi
+fi
+
 rm -rf /tmp/tft
