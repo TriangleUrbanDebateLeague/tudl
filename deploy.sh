d35876cdfdf9b7be6d908e1392a4755be1eef0a4
Fox Wilson <fwilson@fwilson.me>
2016-04-15 20:44:07 -0400

    fix footer

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 git clone $(pwd) /tmp/tft
 (cd /tmp/tft
 git checkout testing
-rsync -rv --exclude .git site fwilson42_tft-test@ssh.phx.nearlyfreespeech.net:/home/protected)
+rsync -rv --exclude .git site nfsn.sh fwilson42_tft-test@ssh.phx.nearlyfreespeech.net:/home/protected)
 rm -rf /tmp/tft
