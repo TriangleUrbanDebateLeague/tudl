be1968ee206bcc39a72dd4020408fcaca460f30f
Fox Wilson <fwilson@fwilson.me>
2016-04-16 00:46:38 -0400

    formatting changes

--- site/app.py
+++ site/app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template, flash, redirect
 from database import database
 
+import logging
+log_formatter = logging.Formatter('''
+Message type:       %(levelname)s
+Location:           %(pathname)s:%(lineno)d
+Module:             %(module)s
+Function:           %(funcName)s
+Time:               %(asctime)s
+
+Message:
+
+%(message)s
+''')
+
 def create_app(environment):
     app = Flask(__name__)
     app.config.from_pyfile("config/{}.py".format(environment))
@@ -8,10 +21,10 @@ def create_app(environment):
     database.init(app.config["DB_PATH"])
 
     if app.config["EMAIL_ERRORS"]:
-        import logging
         from logging.handlers import SMTPHandler
         mail_handler = SMTPHandler('127.0.0.1', app.config["EMAIL_FROM"], app.config["SITE_ADMIN"], 'Exception in TFT-{}'.format(environment))
         mail_handler.setLevel(logging.ERROR)
+        mail_handler.setFormatter(log_formatter)
         app.logger.addHandler(mail_handler)
 
     from modules.account.blueprint import account
