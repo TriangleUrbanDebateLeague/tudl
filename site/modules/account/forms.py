ec18c7d337f58ef3680f979162fa9b80d8f0101e
Fox Wilson <fwilson@fwilson.me>
2016-05-05 07:19:35 -0400

    account: require full street address for registration

--- site/modules/account/forms.py
+++ site/modules/account/forms.py
@@ -9,8 +9,13 @@ class AccountCreateForm(Form):
     email = EmailField('Email', [validators.Required(), validators.Email(), validators.Length(max=64)])
     password = PasswordField('Password', [validators.Required(), validators.Length(min=5, max=92),
                                           validators.EqualTo('confirm', message='Passwords must match!')])
-    state = StringField('State', [validators.Required(), validators.Length(min=2, max=2)])
     confirm = PasswordField('Confirm password')
+
+    street_address = StringField('Street address', [validators.Required(), validators.Length(max=128)])
+    city = StringField('City', [validators.Required(), validators.Length(max=64)])
+    state = StringField('State', [validators.Required(), validators.Length(min=2, max=2)])
+    postal_code = StringField('ZIP code', [validators.Required(), validators.Regexp(r'\d{5}')])
+
 
 class AccountLoginForm(Form):
     email = EmailField('Email', [validators.Required(), validators.Email(), validators.Length(max=64)])
