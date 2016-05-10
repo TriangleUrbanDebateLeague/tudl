9745f188b6fc47aae9effce50d06ab6c4a14b395
Fox Wilson <fwilson@fwilson.me>
2016-05-10 07:19:20 -0400

    simplify deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,12 +5,10 @@ if test $1 = test; then
     (cd /tmp/tft
     git checkout testing
     rsync -rv --exclude .git site nfsn.sh fwilson42_tft-test@ssh.phx.nearlyfreespeech.net:/home/protected)
-else
-    if test $1 = prod; then
-        (cd /tmp/tft
-        git checkout master
-        rsync -rv --exclude .git site nfsn.sh fwilson42_tft-production@ssh.phx.nearlyfreespeech.net:/home/protected)
-    fi
+elif test $1 = prod; then
+    (cd /tmp/tft
+    git checkout master
+    rsync -rv --exclude .git site nfsn.sh fwilson42_tft-production@ssh.phx.nearlyfreespeech.net:/home/protected)
 fi
 
 rm -rf /tmp/tft
