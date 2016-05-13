51162554e84347c36b0be0ec7a349da37c42a6fa
Fox Wilson <fwilson@fwilson.me>
2016-05-13 16:41:52 -0400

    fix dev secret_key

--- site/config/dev.py
+++ site/config/dev.py
@@ -4,7 +4,6 @@ SEND_EMAIL = False
 APP_NAME = "Teens for Teens"
 SECRET_KEY = "key"
 
-SECRET_KEY = os.getenv("SECRET_KEY", os.urandom(32))
 STRIPE_KEY_SECRET = os.getenv("STRIPE_KEY_SECRET", "")
 STRIPE_KEY_PUBLIC = os.getenv("STRIPE_KEY_PUBLIC", "")
 
