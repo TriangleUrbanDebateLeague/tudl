b6dad2e287c0aaf6df04c13071bd28e7fc447c85
Fox Wilson <fwilson@treetrunks.local.fwilson.me>
2016-07-06 19:32:53 -0500

    security: find user

--- site/modules/security/blueprint.py
+++ site/modules/security/blueprint.py
@@ -1,6 +1,7 @@
 from .decorators import require_permission
+from .forms import UserSearchForm
 from .models import Permission
-from flask import Blueprint, current_app, render_template
+from flask import Blueprint, current_app, render_template, request
 from modules.account.models import Account
 
 security = Blueprint("security", __name__, template_folder="templates", url_prefix="/security")
@@ -11,21 +12,36 @@ def security_index():
     modules = sorted(list(current_app.blueprints.keys()))
     return render_template("security/index.html", modules=modules)
 
-@security.route("/query/all/")
+@security.route("/query/permissions/all/")
 @require_permission("security", "access")
 def all_permissions():
     permissions = Permission.select(Permission, Account).join(Account)
     return render_template("security/permissions.html", header="All permissions", permissions=permissions)
 
-@security.route("/query/module/<module>/")
+@security.route("/query/permissions/module/<module>/")
 @require_permission("security", "access")
 def permissions_for_module(module):
     permissions = Permission.select(Permission, Account).where(Permission.module == module).join(Account)
     return render_template("security/permissions.html", header="Permissions for module: {}".format(module), permissions=permissions)
 
-@security.route("/query/user/<int:uid>/")
+@security.route("/query/permissions/user/<int:uid>/")
 @require_permission("security", "access")
 def permissions_for_user(uid):
     account = Account.get(id=uid)
     permissions = account.permissions
     return render_template("security/permissions.html", header="Permissions for user: {}".format(account.full_name), permissions=permissions)
+
+@security.route("/query/user/", methods=["GET", "POST"])
+@require_permission("security", "access")
+def find_user():
+    form = UserSearchForm(request.form)
+
+    if not form.validate_on_submit():
+        return render_template("security/find_user.html", form=form)
+
+    query = Account.select().where(
+            (Account.first_name ** "%{}%".format(form.search.data))
+          | (Account.last_name ** "%{}%".format(form.search.data))
+          | (Account.email == form.search.data)
+    )
+    return render_template("security/users.html", results=query)
