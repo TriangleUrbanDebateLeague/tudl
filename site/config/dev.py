e1086dbdeb8fe0ba9ecf26717aaa8b5e22b02b4c
Fox Wilson <fwilson@treetrunks.local.fwilson.me>
2016-07-14 22:10:30 -0500

    mock out email

--- site/config/dev.py
+++ site/config/dev.py
@@ -1,6 +1,7 @@
 import os
 
-SEND_EMAIL = False
+SEND_EMAIL = True
+EMAIL_FROM = "dev@local.dev"
 APP_NAME = "Teens for Teens"
 SECRET_KEY = "key"
 
@@ -13,3 +14,9 @@ EMAIL_ERRORS = False
 
 DISPLAY_DEBUG_INFO = False
 ALLOW_RCON = False
+
+def send_email(from_, to, subject, text):
+    print("From: {}".format(from_))
+    print("To: {}".format(to))
+    print("Subject: {}".format(subject))
+    print("Text: {}".format(text))
