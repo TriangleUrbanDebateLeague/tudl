ce78a9bc6c2438785a484b4fde44cf7f945b9644
Fox Wilson <fwilson@fwilson.me>
2016-07-24 17:14:39 -0400

    show real data

--- site/modules/staticpages/blueprint.py
+++ site/modules/staticpages/blueprint.py
@@ -1,7 +1,14 @@
 from flask import Blueprint, render_template, make_response
 from jinja2 import TemplateNotFound
 
+from peewee import fn
+from modules.volunteer.models import Volunteer, LoggedHours
+
 staticpages = Blueprint("staticpages", __name__, template_folder="templates", url_prefix="")
+
+@staticpages.context_processor
+def expose_models():
+    return dict(Volunteer=Volunteer, LoggedHours=LoggedHours, fn=fn)
 
 @staticpages.route("/", defaults={"page": "index"})
 @staticpages.route("/<page>/")
