ee2e73560d11b0c280045f1afc075523e5744395
Fox Wilson <fwilson@fwilson.me>
2016-07-25 16:32:18 -0400

    fix migration recursion error

--- site/migrations/001_add_occupation.py
+++ site/migrations/001_add_occupation.py
@@ -1,6 +1,6 @@
 from playhouse.migrate import *
 from modules.donations.models import Donation
 
-def migrate(db):
+def run(db):
     migrator = SqliteMigrator(db)
     migrate(migrator.add_column('donation', 'occupation', Donation.occupation))
