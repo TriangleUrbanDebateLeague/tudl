949a01ae10de01ac4bec07a5bd42be1de38f1471
Omkar Kulkarni <2019okulkarn@tjhsst.edu>
2016-07-27 12:31:01 -0400

    application form

--- site/modules/states/blueprint.py
+++ site/modules/states/blueprint.py
@@ -1,8 +1,10 @@
-from flask import Blueprint, render_template, make_response
+from flask import Blueprint, render_template, make_response, current_app, flash, url_for, request
 from jinja2 import TemplateNotFound
+from collections import OrderedDict
+from .forms import ApplyStateForm
+from utils import flash_errors, send_email
 import os
 import json
-from collections import OrderedDict
 
 states = Blueprint("states", __name__, template_folder="templates", url_prefix="/states")
 
@@ -14,11 +16,20 @@ with open(os.path.dirname(os.path.realpath(__file__)) + '/templates/states/state
 def main_state_page():
 	return render_template("states/states.html", states=states_list)
 
-@states.route("/<state_code>/")
+@states.route("/<state_code>/", methods=["GET", "POST"])
 def show_state_page(state_code):
     state = state_code.upper()
     try:
         state_name = states_list[state].lower()
         return render_template("states/{}.html".format(state_name))
-    except (KeyError, TemplateNotFound):
+    except TemplateNotFound:
+    	form = ApplyStateForm(request.form)
+    	if not form.validate_on_submit():
+    		flash_errors(form)
+    		return render_template("states/apply.html", form=form)
+    	message = render_template("states/apply_email.html", form=form)
+    	send_email('noreply@unifieddemocracy.org', 'benjamin.burstein@unifieddemocracy.org', 'State Director Application - {}'.format(current_app.config.get("APP_NAME", "Unified Democracy")), message)
+    	flash("Application Successful", "success")
+    	return redirect(url_for("staticpages.show_staticpage", page="index"))
+    except KeyError:
         return make_response(render_template("not_found.html"), 404)
