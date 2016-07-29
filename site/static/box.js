693ff32ae6b4d4b76152e4f611ebf3aec893d873
ovkulkarni <ovkulkarni@users.noreply.github.com>
2016-07-29 09:32:35 -0400

    Revert "Minor Fixes"

--- site/static/box.js
+++ site/static/box.js
@@ -1,16 +1,18 @@
 var things = ["lowering the voting age", "improving political literacy", "reducing voter discrimination", "increasing transparency", "eliminating gerrymandering", "simplifying voting", "Teens for Teens"];
 var index = -1;
 $(function() {
-    $el = $("#what-we-are");
     setInterval(function() {
-        if (++index < things.length) {
-            $el.fadeOut(500, function() {
-                $el.html(things[index] + ".");
-                $el.fadeIn(500);
+        index += 1;
+        var el = $("#what-we-are");
+        if(index < things.length) {
+            el.fadeOut(500, function() {
+                el.html(things[index] + ".");
+                el.fadeIn(500);
             });
-        } else {
-            $el.html("Teens for Teens.");
-            $el.show();
+        }
+        else {
+            el.html("Teens for Teens.");
+            el.show();
         }
     }, 4000);
 });
