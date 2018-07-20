aa2239a18c89a4051dcf780fbbf92fef5a919f32
Medha Gupta <medha4@gmail.com>
2018-07-20 19:06:18 -0400

    add header php include tag

--- realsite/index.php
+++ realsite/index.php
@@ -12,22 +12,9 @@
     </head>
     <body>
         <div id="header">
-            <a href="TODO">
-                <img alt="TFT Logo" src="images/Logos/logo.png" class="header-logo-image">
-            </a>
-            <div id="toggleButton">
-                <i class="fa fa-bars" aria-hidden="true"></i>
-            </div>
-            <ul class="header-nav">
-                <li class="header-tab"><a href="TODO">Home</a></li>
-                <li class="header-tab"><a href="TODO">Mission</a></li>
-                <li class="header-tab"><a href="TODO">Volunteer</a></li>
-                <li class="header-tab"><a href="TODO">About Us</a></li>
-                <li class="header-tab"><a href="TODO">Tournament Schedule</a></li>
-                <li class="header-tab"><a href="TODO"><div class="contribute-button">Donate</div></a></li>
-            </ul>
+           <?php include 'realsite/header.php';?>
         </div>
-
+        
 		<div id="huge-header" style="background-image: url(images/backgrounds/memorial.jpg)">
 			<h1>Sample Page</h1>
 		</div>
