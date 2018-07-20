c051029c9d736fad5b98553f487d236fd0ae44da
Medha Gupta <medha4@gmail.com>
2018-07-20 19:09:02 -0400

    add header.php

---
+++ realsite/header.php
@@ -0,0 +1,18 @@
+<?php
+<a href="TODO">
+    <img alt="TFT Logo" src="images/Logos/logo.png" class="header-logo-image">
+</a>
+
+<div id="toggleButton">
+    <i class="fa fa-bars" aria-hidden="true"></i>
+</div>
+
+<ul class="header-nav">
+    <li class="header-tab"><a href="TODO">Home</a></li>
+    <li class="header-tab"><a href="TODO">Mission</a></li>
+    <li class="header-tab"><a href="TODO">Volunteer</a></li>
+    <li class="header-tab"><a href="TODO">About Us</a></li>
+    <li class="header-tab"><a href="TODO">Tournament Schedule</a></li>
+    <li class="header-tab"><a href="TODO"><div class="contribute-button">Donate</div></a></li>
+</ul>
+?>
