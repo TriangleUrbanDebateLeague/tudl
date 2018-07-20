0942ff5c42b43cf14166e627fb9bba51552a61c0
Medha Gupta <medha4@gmail.com>
2018-07-20 19:10:47 -0400

    add footer.php include tag

--- realsite/index.php
+++ realsite/index.php
@@ -54,42 +54,7 @@
         </div>
 
         <div id="footer">
-            <div class="footer-text">
-                <div class="pure-g">
-                    <div class="pure-u-1">
-                        <div class="center">
-                            <img alt="TUDL Logo" src="images/Logos/logo_reduced.png" class="small-logo">
-                        </div>
-                    </div>
-                    
-                    <div class="pure-u-xl-1-4 pure-u-1-2 footer-button">
-                        <a href="TODO"></i>&nbsp;&nbsp;Follow on Google+</a>
-                    </div>
-                    <div class="pure-u-xl-1-4 pure-u-1-2 footer-button">
-                        <a href="TODO"></i>&nbsp;&nbsp;(855) 939 2013</a>
-                    </div>
-                    <div class="pure-u-xl-1-4 pure-u-1-2 footer-button">
-                        <a href="TODO"></i>&nbsp;&nbsp;Follow on Facebook</a>
-                    </div>
-                    <div class="pure-u-xl-1-4 pure-u-1-2 footer-button text-normal">
-                        <a href="TODO"></i>&nbsp;&nbsp;info@<wbr>tudl<wbr>.org</a>
-                    </div>
-                    
-                    <div class="pure-u-lg-1-4 pure-u-1"></div>
-                    <div class="pure-u-lg-1-2 pure-u-1">
-                        <div class="footer-disclaimer text-normal">
-                            Developed by The Triangle Urban Debate League Team.
-                        </div>
-                    </div>
-                    <div class="pure-u-lg-1-1 pure-u-1"></div>
-                    
-                    <div class="pure-u-1-1 footer-links">
-                        <a href="TODO">Mission</a> 
-                        <a href="TODO">Privacy&nbsp;Policy</a>
-                        <a href="TODO">Tournament Schedule</a> 
-                    </div>
-                </div>
-            </div>
+            <?php include 'realsite/footer.php';?>
         </div>
         
         <script type="text/javascript">
