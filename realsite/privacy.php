620b9008840d684be6f2a9b4426499604a963105
Medha Gupta <medha4@gmail.com>
2018-07-28 17:23:50 -0400

    add privacy policy page

---
+++ realsite/privacy.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Triangle Urban Debate League</title>
+        <?php include 'head.php';?>
+    </head>
+    <body>
+        <?php include 'header.php';?>
+
+		<div id="huge-header" style="background-image: url(images/backgrounds/constitution.jpg)">
+			<h1>Privacy Policy</h1>
+		</div>
+
+        <div id="content">
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+        </div>
+		
+		<?php include 'footer.php';?>
+    </body>
+</html>
