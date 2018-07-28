39ca86651ba01f2ee9cd1e42d397a3b437e6705e
Adam <zealan@gmail.com>
2018-07-27 23:48:14 -0400

    add in some spaces for bios and styling

--- realsite/script/about.js
+++ realsite/script/about.js
@@ -2,6 +2,7 @@ var tabs = ["officers", "board"];
 
 hideAllTabs();
 document.getElementById(tabs[0] + "-tab").style.display = "block";
+document.getElementById(tabs[0] + "-select").classList.add("selected");
 
 for (var i in tabs) {
 	setOnClick(tabs[i]);
@@ -11,6 +12,7 @@ function setOnClick(id) {
 	document.getElementById(id + "-select").onclick = function() {
 		hideAllTabs();
 		document.getElementById(id + "-tab").style.display = "block";
+		document.getElementById(id + "-select").classList.add("selected");
 	}
 }
 	
@@ -18,5 +20,6 @@ function setOnClick(id) {
 function hideAllTabs() {
 	for (var i in tabs) {
 		document.getElementById(tabs[i] + "-tab").style.display = "none";
+		document.getElementById(tabs[i] + "-select").classList.remove("selected");
 	}
 }
